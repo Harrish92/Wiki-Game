2673fc5bde481b249fc2458648b608a807cfee46
Harrishan <harrishan.raveendran@epfl.ch>
2025-01-09 13:13:58 +0100

    feat: add button to change the start and end wikipedia page article

--- app/script.js
+++ app/script.js
@@ -7,27 +7,32 @@
     MIT License
 */
 
-var url = "https://en.wikipedia.org/w/api.php"; 
+function get_random(){
+    var url = "https://en.wikipedia.org/w/api.php"; 
 
-var params = {
-    action: "query",
-    format: "json",
-    list: "random",
-    rnlimit: "2",
-    rnnamespace : "0"
-};
+    var params = {
+        action: "query",
+        format: "json",
+        list: "random",
+        rnlimit: "2",
+        rnnamespace : "0"
+    };
+    
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+    
+    fetch(url)
+        .then(function(response){return response.json();})
+        .then(function(response) {
+            var randoms = response.query.random;
+            for (var r in randoms) {
+                console.log(randoms[r].title);
+            }
+            document.getElementById("start").textContent = randoms[0].title;
+            document.getElementById("end").textContent = randoms[1].title;
+        })
+        .catch(function(error){console.log(error);})
+}
 
-url = url + "?origin=*";
-Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
-
-fetch(url)
-    .then(function(response){return response.json();})
-    .then(function(response) {
-        var randoms = response.query.random;
-        for (var r in randoms) {
-            console.log(randoms[r].title);
-        }
-        document.getElementById("start").textContent = randoms[0].title;
-        document.getElementById("end").textContent = randoms[1].title;
-    })
-    .catch(function(error){console.log(error);})
+get_random();
+document.getElementById("change").onclick = get_random;
