088c219ac0c0351b776d9265464dafaedc941694
Harrishan <harrishan.raveendran@epfl.ch>
2025-01-09 02:46:37 +0100

    feat: fetch two random wikipedia page articles

--- app/script.js
+++ app/script.js
@@ -1,7 +1,33 @@
+/*
+    get_random.js
 
-function setup() {
-    userInput = document.getElementById("userinput").value;
-    console.log(userInput);
-}
+    MediaWiki API Demos
+    Demo of `Random` module: Get request to list 5 random pages.
 
-setup();
+    MIT License
+*/
+
+var url = "https://en.wikipedia.org/w/api.php"; 
+
+var params = {
+    action: "query",
+    format: "json",
+    list: "random",
+    rnlimit: "2",
+    rnnamespace : "0"
+};
+
+url = url + "?origin=*";
+Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+fetch(url)
+    .then(function(response){return response.json();})
+    .then(function(response) {
+        var randoms = response.query.random;
+        for (var r in randoms) {
+            console.log(randoms[r].title);
+        }
+        document.getElementById("start").textContent = randoms[0].title;
+        document.getElementById("end").textContent = randoms[1].title;
+    })
+    .catch(function(error){console.log(error);})
